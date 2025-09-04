1dcbdac006761df11932b3a92965aa6a12633560
Shashank Kumar dev <shashankkumardev@gmail.com>
2025-09-04 17:27:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+gsap.registerPlugin(ScrollTrigger);
+Shery.mouseFollower();
+Shery.makeMagnet(".magnet");
+Shery.hoverWithMediaCircle(".hvr", { videos: ["./0.mp4", "./2.mp4", "./3.mp4"],});
+
+gsap.to(".fleftelm", {
+  scrollTrigger: {
+    trigger: ".fimages",
+    pin: true,
+    start: "top top",
+    end: "bottom bottom",
+    endTrigger: ".last",
+    scrub: 1,
+  },
+  y: "-300%",
+  ease: Power1.inOut,
+});
+Shery.imageEffect(".images", {
+  style: 5,
+
+  slideStyle: (setScroll) => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY / innerHeight); 
+    });
+  },
+});
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+Shery.mouseFollower();
+Shery.makeMagnet(".magnet");
+Shery.hoverWithMediaCircle(".hvr", {
+  videos: ["./0.mp4", "./2.mp4", "./3.mp4"],
+});
